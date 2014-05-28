5a3aa47cc89fa5ec904b33e434e56ea514aae566
Federico <f.casali@gmail.com>
2014-05-28 14:34:54 -0700

    Styles_3000 & 3001 sample code update

--- test/apps/Alloy-Styles/Styles_3001_Global-Controller-Platform-Inline/controllers/index.js
+++ test/apps/Alloy-Styles/Styles_3001_Global-Controller-Platform-Inline/controllers/index.js
@@ -2,4 +2,16 @@ if (OS_IOS){
 	alert('iOS!');
 }
 
+else if (OS_ANDROID){
+	alert('Android!')
+}
+
+else if (OS_MOBILEWEB){
+	alert('Mobileweb!')
+}
+
+else {
+	alert("Not iOS, Android or MobWeb")
+}
+
 $.index.open();
