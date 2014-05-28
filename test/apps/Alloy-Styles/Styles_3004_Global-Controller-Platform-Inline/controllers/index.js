ccd5de4fd3aa24abfdb1bb76adbb44159eeab9ff
Federico <f.casali@gmail.com>
2014-05-28 15:58:03 -0700

    Styles_3004 updated test case

--- test/apps/Alloy-Styles/Styles_3004_Global-Controller-Platform-Inline/controllers/index.js
+++ test/apps/Alloy-Styles/Styles_3004_Global-Controller-Platform-Inline/controllers/index.js
@@ -2,6 +2,6 @@ function doClick(e) {
     Alloy.createController('win1').getView().open();
 }
 
-$.button.title='Width: ' + $.button.width
+$.button.title='Click! (Width: ' + $.button.width + ')';
  
 $.mainWin.open();
